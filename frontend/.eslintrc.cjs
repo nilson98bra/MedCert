dd49c667d370d36d8838c3576abbb4534877e962
nilson98bra <nilson.98@hotmail.com>
2025-08-21 21:16:49 -0300

    chore: add eslint and prettier

---
+++ frontend/.eslintrc.cjs
@@ -0,0 +1,23 @@
+/* eslint-env node */
+require("@rushstack/eslint-patch/modern-module-resolution");
+
+module.exports = {
+  root: true,
+  extends: [
+    "plugin:vue/vue3-essential",
+    "eslint:recommended",
+    "@vue/eslint-config-typescript",
+    "@vue/eslint-config-prettier",
+  ],
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  parserOptions: {
+    ecmaVersion: "latest",
+  },
+  rules: {
+    "vue/multi-word-component-names": "off",
+  },
+};
